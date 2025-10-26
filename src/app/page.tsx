8981c4b22d4f0ca0e249bc730375506d84e9abbc
bibhashdash <b.dash@12os.co.uk>
2025-10-26 19:50:31 +0000

    initial commit 2

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,7 +2,7 @@ export default function Home() {
   return (
     <div className="flex min-h-screen items-center justify-center bg-zinc-50 font-sans">
       <main className="flex min-h-screen w-full flex-col items-center justify-between bg-white sm:items-start">
-       <p className="text-black">Hello World</p>
+       <p className="text-black">BAE Showcase App</p>
       </main>
     </div>
   );
